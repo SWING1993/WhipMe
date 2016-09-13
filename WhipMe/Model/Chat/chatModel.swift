1262142884621c6849590f3cbc37cd1ae9a1a18e
anve <youye@youye-523.local>
2016-09-13 14:43:19 +0800

    dd

---
+++ WhipMe/Model/Chat/chatModel.swift
@@ -0,0 +1,13 @@
+//
+//  chatModel.swift
+//  WhipMe
+//
+//  Created by anve on 16/9/13.
+//  Copyright © 2016年 -. All rights reserved.
+//
+
+import Foundation
+
+class chatModel: NSObject {
+
+}
---
+++ WhipMe/Views/Chat/chatView.swift
@@ -0,0 +1,13 @@
+//
+//  chatView.swift
+//  WhipMe
+//
+//  Created by anve on 16/9/13.
+//  Copyright © 2016年 -. All rights reserved.
+//
+
+import UIKit
+
+class chatView: UIView {
+
+}
