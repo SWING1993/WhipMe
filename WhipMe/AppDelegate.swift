8e9f1b4117f659e909f27d2dace90e9a439b6f0b
宋国华 <10446627@qq.com>
2017-09-19 14:53:48 +0800

    速速花

--- WhipMe/AppDelegate.swift
+++ WhipMe/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        // 速速花
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
         self.setupIndexWebController()
         HttpAPIClient.setupXHLaunchAd()
