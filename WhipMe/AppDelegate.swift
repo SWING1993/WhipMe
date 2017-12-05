323bd3d1ceb01e16189821f67a60898537682243
宋国华 <10446627@qq.com>
2017-12-05 10:19:45 +0800

    app图

--- WhipMe/AppDelegate.swift
+++ WhipMe/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // 速速花
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
         self.setupIndexWebController()
-        HttpAPIClient.setupXHLaunchAd()
+//        HttpAPIClient.setupXHLaunchAd()
         HttpAPIClient.startIndexSuccess({ (result) in
             if let dataResult = result {
                 let json = JSON(dataResult)
